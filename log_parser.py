542a926bf4a2bf5c5b55cbaa7a5b6edd701b1824
asqiriba <h.asqiriba@pm.me>
2021-10-13 09:58:16 -0700

    add data digestion functions

--- log_parser.py
+++ log_parser.py
@@ -1,14 +1,47 @@
 """
 """
+import os.path
 
-packets = []
-digital_outputs = []
+class Parser:
+    def __init__(self, path) -> None:
+        self.packets = []
+        self.digital_outputs = []
 
-header = "Digi XBee3 Zigbee"
-path = "COMP596/api_console_session_2021-10-12.log"
+        self._header = "Digi XBee3 Zigbee"
+        self.path = path
 
-with open(path) as file:
-    for line in file:
-        if(header in line or line == "\n"):
-            continue
-        packets.append(line)
+    def read_packets(self):
+        file_exists = os.path.exists(self.path)
+
+        if(not file_exists):
+            print("File not found. Check path.")
+            return
+
+        with open(self.path) as file:
+            for line in file:
+                if(self._header in line or line == "\n"):
+                    continue
+
+                self.packets.append(line.split(","))
+        
+        self._get_hex()
+    
+    def _get_hex(self):
+        for date, index, api, hex in self.packets:
+            self.digital_outputs.append(hex)
+
+    def _get_api(self):
+        for date, index, api, hex in self.packets:
+            self.digital_outputs.append(api)
+    
+    def _get_index(self):
+        for date, index, api, hex in self.packets:
+            self.digital_outputs.append(index)
+    
+    def _get_date(self):
+        for date, index, api, hex in self.packets:
+            self.digital_outputs.append(date)
+
+if(__name__ == "__main__"):
+    parser = Parser("COMP596/api_console_session_2021-10-12.log")
+    parser.read_packets()
